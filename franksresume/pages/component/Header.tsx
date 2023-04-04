3c067dbaaee8a589dbe85e25b563563bbcf7efe7
Syed Zahid <115338908+Frankzed110@users.noreply.github.com>
2023-04-04 05:53:01 +0500

    Effects - added

--- franksresume/pages/component/Header.tsx
+++ franksresume/pages/component/Header.tsx
@@ -6,11 +6,11 @@ export default function Header() {
       <div className='flex justify-between'>
       <h1 className='font-SignatureName m-8'>ZAHID HUSSAIN</h1>
    <ul className='m-7 flex'>
-  <li className='mx-5 text-2xl font-SignatureName'> Home </li>
-  <li className='mx-5 text-2xl font-SignatureName'> About </li>
-  <li className='mx-5 text-2xl font-SignatureName'> Portfolio </li>
-  <li className='mx-5 text-2xl font-SignatureName'> Skills </li>
-  <li className='mx-5 text-2xl font-SignatureName'> Contact </li>
+  <li className='mx-5 text-2xl font-SignatureName hover:scale-125 duration-300'> Home </li>
+  <li className='mx-5 text-2xl font-SignatureName hover:scale-125 duration-300'> About </li>
+  <li className='mx-5 text-2xl font-SignatureName hover:scale-125 duration-300'> Portfolio </li>
+  <li className='mx-5 text-2xl font-SignatureName hover:scale-125 duration-300'> Skills </li>
+  <li className='mx-5 text-2xl font-SignatureName hover:scale-125 duration-300'> Contact </li>
    </ul>
    </div>
     </section>
